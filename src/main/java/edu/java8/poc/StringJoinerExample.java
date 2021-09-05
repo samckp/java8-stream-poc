c8feaad2d0f9b72fd04dfda77181a00e6d8d4328
samckp <sam.mca.liet@gmail.com>
2021-09-05 20:56:17 +0530

    code updated

--- src/main/java/edu/java8/poc/StringJoinerExample.java
+++ src/main/java/edu/java8/poc/StringJoinerExample.java
@@ -1,10 +1,22 @@
 package edu.java8.poc;
 
 import java.util.List;
+import java.util.StringJoiner;
+
 public class StringJoinerExample {
 
     public static void main(String[] args) {
-         
 
+        // Passing Hyphen(-) as delimiter
+        StringJoiner str = new StringJoiner("-");
+
+        // Joining multiple strings by using add() method
+        str.add("Apple");
+        str.add("Mango");
+        str.add("Coconut");
+        str.add("Pineapple");
+
+        // Displaying the output String
+        System.out.println(str);
     }
 }
