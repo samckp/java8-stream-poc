4393157e6c2703982f3e25497a57aa5a5f7106ce
samckp <sam.mca.liet@gmail.com>
2021-09-08 07:51:20 +0530

    updated

--- src/main/java/edu/java8/poc/StreamExamples.java
+++ src/main/java/edu/java8/poc/StreamExamples.java
@@ -1,2 +1,23 @@
-package edu.java8.poc;public class StreamExamples {
+package edu.java8.poc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamExamples {
+
+    public static void main(String[] args) {
+
+        List<String> names = Arrays.asList("howard", "camp", "oxford", "Mellon", "Bits");
+
+        String result = names.stream()
+                .collect(Collectors.joining("#\n"));
+
+        System.out.println(result);
+
+        //numbers
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        
+        
+    }
 }
