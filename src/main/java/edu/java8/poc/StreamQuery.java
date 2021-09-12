c7958b412eacb261fc4c5684581e3e77d2065ee2
samckp <sam.mca.liet@gmail.com>
2021-09-12 08:06:27 +0530

    added

--- src/main/java/edu/java8/poc/StreamQuery.java
+++ src/main/java/edu/java8/poc/StreamQuery.java
@@ -1,9 +1,15 @@
 package edu.java8.poc;
 
+import java.util.stream.Stream;
+
 public class StreamQuery {
-    
+
     public static void main(String[] args) {
 
+        Stream.iterate(1, num -> num +1)
+                .filter(n->n%2==0)
+                .limit(10)
+                .forEach(System.out::println);
 
     }
 }
