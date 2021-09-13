25650f78bdd91e31b482f2ae7570947b71812cf7
samckp <sam.mca.liet@gmail.com>
2021-09-13 07:53:19 +0530

    stream example added

--- src/main/java/edu/java8/poc/StreamExamples.java
+++ src/main/java/edu/java8/poc/StreamExamples.java
@@ -15,9 +15,42 @@ public class StreamExamples {
 
         System.out.println(result);
 
-        //numbers
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        // Print the elements in a order
+        System.out.println("Ordered form :: ");
+        names.stream()
+                .forEachOrdered(name -> System.out.println(name))
+        ;
 
-//        numbers.stream().map().
+
+        //numbers
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        // Print the elements of Collection
+        numbers.stream()
+                .forEach(System.out::println );
+
+        // Filter the numbers
+        numbers.stream()
+                .filter(n->n%2==1)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+        /// Sum of numbers from a collection
+        Integer sum = numbers.stream()
+                        .collect(Collectors.summingInt(x -> x));
+
+        System.out.println("Sum :: " + sum);
+
+        // Find the Average of number.
+        Double avg = numbers.stream()
+                .collect(Collectors.averagingInt(n-> n));
+
+        System.out.println("Average :: " + avg);
+
+        //Count the number of elements in stream
+        Long count = numbers.stream()
+                .collect(Collectors.counting())
+                ;
+        System.out.println("Count :: " + count);
     }
 }
