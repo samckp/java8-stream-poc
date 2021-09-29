92af8a00b29a0ecdd3fc1b6f9021daa9865812a6
samckp <sam.mca.liet@gmail.com>
2021-09-29 07:32:25 +0530

    added prefix checker

---
+++ src/main/java/edu/java8/poc/LongestPrefixChecker.java
@@ -0,0 +1,47 @@
+package edu.java8.poc;
+
+import java.util.Arrays;
+
+public class LongestPrefixChecker {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"mint", "mini", "mineral"};  //{"keeping","keepers","keeptyr"}
+
+        System.out.println("longest prefix :: " + prefixChecker(arr));
+    }
+
+    public static String prefixChecker(String[] input){
+
+        Arrays.sort(input);
+
+        int size = input.length;
+        int length = input[0].length();
+
+        StringBuilder res = new StringBuilder();
+
+        for(int i=0;i<length;i++){
+
+            if(input[0].charAt(i) == input[size -1].charAt(i)){
+
+                res.append(input[0].charAt(i));
+            } else {
+
+                break;
+            }
+        }
+
+        return res.toString();
+    }
+}
+
+//Algorithm
+
+//1. Sort the array of strings in alphabetical order.
+//
+//Compare the characters in the first and last strings in the array. Since the array is sorted,
+//common characters among the first and last element will be common among all the elements of the array.
+//
+//2.1. If they are same, then append the character to the result.
+//
+//2.2. Else, stop the comparison – result contains the longest common prefix among the strings in the array.
