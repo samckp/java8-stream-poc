fde986e3a2a1aa9c1a09b4089aaed72b37bbc75b
samckp <sam.mca.liet@gmail.com>
2021-10-06 09:16:46 +0530

    updated

--- src/main/java/edu/java8/poc/AbstrctDemo.java
+++ src/main/java/edu/java8/poc/AbstrctDemo.java
@@ -1,2 +1,30 @@
-package edu.java8.poc;public class AbstrctDemo {
+package edu.java8.poc;
+
+abstract class demo{
+
+    int a;
+    demo(){
+        a =10;
+    }
+    abstract void set(int a);
+    abstract void get();
+}
+public class AbstrctDemo extends demo {
+
+    @Override
+    void set(int a) {
+        this.a = a;
+    }
+
+    @Override
+    void get() {
+        System.out.println("a = " + a);
+    }
+
+    public static void main(String[] args) {
+
+        AbstrctDemo  abstrctDemo = new AbstrctDemo();
+        abstrctDemo.set(20);
+        abstrctDemo.get();
+    }
 }
--- src/main/java/edu/java8/poc/DefaultMethodEx.java
+++ src/main/java/edu/java8/poc/DefaultMethodEx.java
@@ -1,2 +1,44 @@
-package edu.java8.poc;public class DefaultMethodEx {
+package edu.java8.poc;
+
+class Parent{
+
+    public void greet(){
+        System.out.println("Hello from Parent class method !!");
+    }
+}
+interface Test{
+
+    public default void greet(){
+        System.out.println("Hello from Test interface default method !!");
+    }
+
+    static void hello(){
+        System.out.println("hello from Test interface static method !!");
+    }
+}
+interface Test1 {
+
+    public default void greet(){
+        System.out.println("Hello from Test1 interface default method !!");
+    }
+    static void hello(){
+        System.out.println("hello from Test1 interface static method !!");
+    }
+}
+public class DefaultMethodEx extends Parent implements Test , Test1{
+
+    public void greet(){
+
+        super.greet();
+        Test.super.greet();
+        Test1.super.greet();
+    }
+
+    public static void main(String[] args) {
+
+        new DefaultMethodEx().greet();
+
+        Test.hello();
+        Test1.hello();
+    }
 }
